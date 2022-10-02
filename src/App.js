f370a1e8adaac275329bd9f850cb7f53d536bd4e
lzxzz <1042183935@qq.com>
2022-10-02 15:29:20 +0800

    初学react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{ 
+    constructor(){
+        super();
+        this.state={
+            n:0
+        }
+    }
+    add(){
+        this.setState((state)=>{
+            const n = state.n +1
+            return {n}
+        }) //监听   是异步任务 所以用函数的形式
+    }
+    render(){
+        return (
+        <div>
+            <div>爸爸:{this.state.n}</div>
+            <button onClick={()=>this.add()}>+1</button>
+            <Son/>
+        </div>
+    )
+    }
 }
-
-export default App;
+function Son (){
+    const [n,setN] = React.useState(0)
+    const [m,setM] = React.useState(0)
+    return (
+        <div><br/>
+            儿子:{n}
+            <button onClick={()=>setN(n+1)}>+1</button>
+            <br/>
+            儿子:{m}
+            <button onClick={()=>setM(m+1)}>+1</button>
+        </div>
+    )
+}
+export default App
